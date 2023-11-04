aae56a2716090e20454016fc2b26b34272253990
WW810713 <75856731+WW810713@users.noreply.github.com>
2023-11-05 00:16:00 +0800

    Add files via upload

---
+++ JS/郎牌特曲.js
@@ -0,0 +1,553 @@
+{
+"spider": "https://tudouaa1.oss-cn-beijing.aliyuncs.com/jar/fan1104.jar;md5;514c44ae1ccc02d41e69326d5eea4242",
+"wallpaper":"http://饭太硬.ga/深色壁纸/api.php",
+"sites":[
+{
+"key":"豆豆",
+"name":"郎牌特曲影院首页",
+"type":3,
+"api":"csp_DouDou",
+"searchable":0,
+"quickSearch":0,
+"filterable":0
+},
+{
+"key": " 短剧┃网",
+"name": "郎牌特曲短剧",
+"type": 3,
+"api": "http://alist.xn--z7x900a.love:63/ddrpy2.m.js",
+"ext": "http://alist.xn--z7x900a.love:63/短剧网.js",
+"gridview": "0-0-H",
+"style": {
+"type": "rect",
+"ratio": 1.68
+}
+},
+{
+"key":"drpy_js_两个BT",
+"name":"鉴赏12超清-1",
+"playerType":1,
+"type":3,"api":"http://106.52.241.21:99/JS/lib/drpy2.min.js",
+"ext":"http://106.52.241.21:99/JS/js/两个BT.js"
+},
+{
+"key":"厂长",
+"name":"鉴赏18超清-2",
+"playerType":1,
+"type":3,
+"api":"csp_XBPQ",
+"searchable":1,
+"quickSearch":1,
+"filterable":1,
+"ext":"https://tudouaa1.oss-cn-beijing.aliyuncs.com/json/cz.json"
+},
+{
+"key":"lurenjia",
+"name":"兼香T9超清-3",
+"playerType":1,
+"type":1,
+"api":"http://dytt996.com/api.php/provide/vod/?ac=list",
+"searchable":1,
+"quickSearch":1,
+"filterable":1,
+"playUrl":"json:https://gg.dytt996.com/quad.php?url="
+},
+{
+"key":"zxzj",
+"name":"兼香T8蓝光-1",
+"playerType":1,
+"type":3,
+"api":"csp_Zxzj",
+"searchable":1,
+"quickSearch":1,
+"changeable":1,
+"ext":"https://www.zxzj.pro/"
+},
+{
+"key":"csp_Bttoo",
+"name":"兼香T6蓝光-2",
+"playerType":1,
+"type":3,
+"api":"csp_Bttwoo",
+"searchable":1,
+"quickSearch":1,
+"changeable":1
+},
+{
+"key":"app_轻简",
+"name":"T9蓝光-3",
+"playerType":1,
+"type":1,
+"api":"http://1.ahailove.cn:23456/cj/api.php/provide/vod/",
+"categories":[
+"电影",
+"电视剧",
+"综艺",
+"动漫"
+]
+},
+{
+"key": "玉米",
+"name": "T8蓝光-4",
+"type": 0,
+"api": "http://124.222.116.5/macos/api.php/provide/vod/at/xml/",
+"playUrl": "json:https://www.qiaoji8.com/tvbox/json.php?url=",
+"searchable": 1,
+"quickSearch": 1,
+"filterable": 1
+},
+{
+"key": "saohuo",
+"name": "T6蓝光-5",
+"type": 3,
+"searchable": 1,
+"quickSearch": 1,
+"playerType": "2",
+"jar": "http://xhww.fun:63/小米/暴脾气.jar;md5;bb155c3f0133bbce4756ad52003f5968",
+"api": "csp_XBPQ",
+"ext": {
+"嗅探词": ".m3u8#.mp4#.m3u8?#freeok.mp4",
+"分类url": "https://shdy2.com/list/{cateId}-{catePg}.html;;r1dau0",
+"分类": "电影$1#电视剧$2#韩剧$22#美剧$23#动漫$4",
+"数组二次截取": "class=\"v_list\">&&</ul>",
+"数组": "\"v_img\"&&/div>",
+"标题": "title=\"&&\"",
+"副标题": "v_note\"&&</div",
+"跳转播放链接": "<iframe*src=\"&&\"",
+"二次跳转播放链接": "https://hkjx.hhplayer.com/api.php;post;url=+var url*\"&&\"+&t=+var t*\"&&\"+&key=+var key*\"&&\"+&act=0+&play=1",
+"三次跳转播放链接": "https://hkjx.hhplayer.com+\"url\"*\"&&\""
+}
+},
+{
+"key":"csp_WoGG",
+"name":"郎牌特曲4K-1",
+"playerType":1,
+"type":3,
+"api":"csp_WoGG",
+"searchable":1,
+"quickSearch":1,
+"changeable":0,
+"ext":"http://127.0.0.1:9978/file/tvfan/token.txt+4k|auto|fhd$$$http://www.wogg.xyz/$$$关"
+},
+{
+"key":"相声小品",
+"name":"相声小品",
+"playerType":1,
+"type":3,
+"api":"csp_Bili",
+"searchable":0,
+"quickSearch":0,
+"filterable":1,
+"ext":"https://tudouaa1.oss-cn-beijing.aliyuncs.com/json/Blixs.json"
+},
+{
+"key":"戏曲",
+"name":"戏曲",
+"playerType":1,
+"type":3,
+"api":"csp_Bili",
+"searchable":0,
+"quickSearch":0,
+"filterable":1,
+"ext":"https://tudouaa1.oss-cn-beijing.aliyuncs.com/json/Blixq.json"
+},
+{"key":"dr_兔小贝",
+"name":"儿童早教",
+"playerType":1,
+"type":3,
+"api":"http://frp.gerss.cn/e4a/app/js/lib/drpy2.min.js",
+"ext":"http://frp.gerss.cn/e4a/app/js/js/兔小贝.js",
+"searchable":0,
+"quickSearch":0,
+"changeable":0
+},
+{
+"key":"少儿",
+"name":"少儿课堂",
+"playerType":1,
+"type":3,
+"api":"csp_Bili",
+"searchable":0,
+"quickSearch":0,
+"filterable":1,
+"ext":"https://tudouaa1.oss-cn-beijing.aliyuncs.com/json/%E5%B0%91%E5%84%BF%E6%95%99%E8%82%B2.json"
+},
+{
+"key":"小学",
+"name":"小学课堂",
+"playerType":1,
+"type":3,
+"api":"csp_Bili",
+"searchable":0,
+"quickSearch":0,
+"filterable":1,
+"ext":"https://tudouaa1.oss-cn-beijing.aliyuncs.com/json/%E5%B0%8F%E5%AD%A6%E8%AF%BE%E5%A0%82.json"
+},
+{
+"key":"初中",
+"name":"初中课堂",
+"playerType":1,
+"type":3,
+"api":"csp_Bili",
+"searchable":0,
+"quickSearch":0,
+"filterable":1,
+"ext":"https://tudouaa1.oss-cn-beijing.aliyuncs.com/json/%E5%88%9D%E4%B8%AD%E8%AF%BE%E5%A0%82.json"
+},
+{
+"key":"高中",
+"name":"高中课堂",
+"playerType":1,
+"type":3,
+"api":"csp_Bili",
+"searchable":0,
+"quickSearch":0,
+"filterable":1,
+"ext":"https://tudouaa1.oss-cn-beijing.aliyuncs.com/json/%E9%AB%98%E4%B8%AD%E8%AF%BE%E5%A0%82.json"
+},
+{
+"key":"csp_Bili",
+"playerType":1,
+"name":"B站",
+"type":3,
+"api":"csp_Bili",
+"searchable":0,
+"quickSearch":0,
+"filterable":1,
+"ext":"https://tudouaa1.oss-cn-beijing.aliyuncs.com/json/Bili.json"
+},
+{
+"key": "88看球",
+"name": "⚽88看球",
+"playerType":1,
+"type": 3,
+"api": "https://tudouaa1.oss-cn-beijing.aliyuncs.com/api/lib/drpy2.min.js",
+"searchable": 0,
+"quickSearch": 0,
+"filterable": 1,
+"ext": "https://tudouaa1.oss-cn-beijing.aliyuncs.com/api/lib/88kanqiu.js"
+},
+{
+"key":"七夜",
+"name":"郎牌特曲-4K备用1",
+"type":3,
+"api":"csp_Dovx",
+"searchable":1,
+"quickSearch":1,
+"changeable":0,
+"ext":"http://127.0.0.1:9978/file/tvfan/token.txt+4k|auto|fhd"
+},
+{
+"key": "csp_search_Alips",
+"name": "郎牌特曲-4K备用2",
+"type": 3,
+"api": "csp_PanSou",
+"searchable": 1,
+"quickSearch": 1,
+"filterable": 0,
+"playerType": 1,
+"ext": "http://127.0.0.1:9978/file/fatcat/token.txt"
+},
+{
+"key":"UpYun",
+"name":"郎牌特曲-4K备用3",
+"type":3,
+"api":"csp_UpYun",
+"searchable":1,
+"quickSearch":1,
+"changeable":0,
+"ext":"http://127.0.0.1:9978/file/tvfan/token.txt+4k|auto|fhd"
+},
+{
+"key":"PanSearch",
+"name":"郎牌特曲-4K备用4",
+"type":3,
+"api":"csp_PanSearch",
+"searchable":1,
+"quickSearch":1,
+"changeable":0,
+"ext":"http://127.0.0.1:9978/file/tvfan/token.txt+4k|auto|fhd"
+}
+
+],
+
+"lives":[
+{
+"group":"redirect",
+"channels":[
+{
+"name":"redirect",
+"urls":[
+"proxy://do=live&type=txt&ext=https://tudouaa1.oss-cn-beijing.aliyuncs.com/api/tv.txt"
+]
+}
+]
+}
+],
+"flags":[],
+"parses":[
+{
+"name":"TA",
+"type":1,
+"url":"https://hd.396pay.cn/api/diy/?key=pFELnzclw6RdhxNlH6&url="
+},
+{
+"name":"TB",
+"type":1,
+"url":"https://api.xn--kcr500a391b.cc/api/jiexi/common?Key=V1JNMfpVhruGmgbj5s&url="
+},
+{
+"name":"TD",
+"type":1,
+"url":"http://119.45.115.156:90/cache.php?url="
+},
+{
+"name":"TC",
+"type":1,
+"url":"https://hd.396pay.cn/api/diy/?key=pFELnzclw6RdhxNlH6&url="
+},
+{
+"name":"TBF",
+"type":1,
+"url":"http://106.53.99.43:84/bf.php?url="
+},
+{
+"name":"TLZ",
+"type":1,
+"url":"http://106.53.99.43:84/lz.php?url="
+},
+{
+"name":"TFF",
+"type":1,
+"url":"http://106.53.99.43:84/ff.php?url="
+},
+{
+"name":"T49",
+"type":1,
+"url":"http://106.53.99.43:84/49.php?url="
+}
+],
+"ijk":[
+{
+"group":"软解码",
+"options":[
+{
+"category":4,
+"name":"opensles",
+"value":"0"
+},
+{
+"category":4,
+"name":"overlay-format",
+"value":"842225234"
+},
+{
+"category":4,
+"name":"framedrop",
+"value":"1"
+},
+{
+"category":4,
+"name":"soundtouch",
+"value":"1"
+},
+{
+"category":4,
+"name":"start-on-prepared",
+"value":"1"
+},
+{
+"category":1,
+"name":"http-detect-range-support",
+"value":"0"
+},
+{
+"category":1,
+"name":"fflags",
+"value":"fastseek"
+},
+{
+"category":2,
+"name":"skip_loop_filter",
+"value":"48"
+},
+{
+"category":4,
+"name":"reconnect",
+"value":"1"
+},
+{
+"category":4,
+"name":"enable-accurate-seek",
+"value":"0"
+},
+{
+"category":4,
+"name":"mediacodec",
+"value":"0"
+},
+{
+"category":4,
+"name":"mediacodec-auto-rotate",
+"value":"0"
+},
+{
+"category":4,
+"name":"mediacodec-handle-resolution-change",
+"value":"0"
+},
+{
+"category":4,
+"name":"mediacodec-hevc",
+"value":"0"
+},
+{
+"category":1,
+"name":"dns_cache_timeout",
+"value":"600000000"
+}
+]
+},
+{
+"group":"硬解码",
+"options":[
+{
+"category":4,
+"name":"opensles",
+"value":"0"
+},
+{
+"category":4,
+"name":"overlay-format",
+"value":"842225234"
+},
+{
+"category":4,
+"name":"framedrop",
+"value":"1"
+},
+{
+"category":4,
+"name":"soundtouch",
+"value":"1"
+},
+{
+"category":4,
+"name":"start-on-prepared",
+"value":"1"
+},
+{
+"category":1,
+"name":"http-detect-range-support",
+"value":"0"
+},
+{
+"category":1,
+"name":"fflags",
+"value":"fastseek"
+},
+{
+"category":2,
+"name":"skip_loop_filter",
+"value":"48"
+},
+{
+"category":4,
+"name":"reconnect",
+"value":"1"
+},
+{
+"category":4,
+"name":"enable-accurate-seek",
+"value":"0"
+},
+{
+"category":4,
+"name":"mediacodec",
+"value":"1"
+},
+{
+"category":4,
+"name":"mediacodec-auto-rotate",
+"value":"1"
+},
+{
+"category":4,
+"name":"mediacodec-handle-resolution-change",
+"value":"1"
+},
+{
+"category":4,
+"name":"mediacodec-hevc",
+"value":"1"
+},
+{
+"category":1,
+"name":"dns_cache_timeout",
+"value":"600000000"
+}
+]
+}
+],
+"ads":[
+"mimg.0c1q0l.cn",
+"www.googletagmanager.com",
+"www.google-analytics.com",
+"mc.usihnbcq.cn",
+"mg.g1mm3d.cn",
+"mscs.svaeuzh.cn",
+"cnzz.hhttm.top",
+"tp.vinuxhome.com",
+"cnzz.mmstat.com",
+"www.baihuillq.com",
+"s23.cnzz.com",
+"z3.cnzz.com",
+"c.cnzz.com",
+"stj.v1vo.top",
+"z12.cnzz.com",
+"img.mosflower.cn",
+"tips.gamevvip.com",
+"ehwe.yhdtns.com",
+"xdn.cqqc3.com",
+"www.jixunkyy.cn",
+"sp.chemacid.cn",
+"hm.baidu.com",
+"s9.cnzz.com",
+"z6.cnzz.com",
+"um.cavuc.com",
+"mav.mavuz.com",
+"wofwk.aoidf3.com",
+"z5.cnzz.com",
+"xc.hubeijieshikj.cn",
+"tj.tianwenhu.com",
+"xg.gars57.cn",
+"k.jinxiuzhilv.com",
+"cdn.bootcss.com",
+"ppl.xunzhuo123.com",
+"xomk.jiangjunmh.top",
+"img.xunzhuo123.com",
+"z1.cnzz.com",
+"v1.cnzz.com",
+"s13.cnzz.com",
+"xg.huataisangao.cn",
+"z7.cnzz.com",
+"xg.huataisangao.cn",
+"z2.cnzz.com",
+"s96.cnzz.com",
+"q11.cnzz.com",
+"thy.dacedsfa.cn",
+"xg.whsbpw.cn",
+"s19.cnzz.com",
+"z8.cnzz.com",
+"s4.cnzz.com",
+"f5w.as12df.top",
+"ae01.alicdn.com",
+"videocloud.cn-hangzhou.log.aliyuncs.com",
+"www.92424.cn",
+"k.wudejia.com",
+"vivovip.mmszxc.top",
+"qiu.xixiqiu.com",
+"cdnjs.hnfenxun.com",
+"cms.qdwght.com"
+]
+}
